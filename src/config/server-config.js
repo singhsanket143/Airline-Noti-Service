53fde7c338862c7b15ee7e5cf864f6b48d0e1ee0
Sanket Singh <singhsanket143@gmail.com>
2023-06-04 13:04:13 +0530

    Created nodemailer transport object and tried

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    GMAIL_PASS: process.env.GMAIL_PASS,
+    GMAIL_EMAIL: process.env.GMAIL_EMAIL
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+const mailsender = require('./config/email-config')
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try {
+        const response = await mailsender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'abc@gmail.com',
+            subject: 'Is the service working ? now as well',
+            text: 'Yes it is working'
+        });
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+    }
 });
